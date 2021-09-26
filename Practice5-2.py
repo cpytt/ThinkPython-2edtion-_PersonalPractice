3449e77ed5ceb4305cbff83de077766ccd6197cd
cpytt <381876024@qq.com>
2021-09-26 23:17:17 +0800

    ----

--- Practice5-2.py
+++ Practice5-2.py
@@ -5,4 +5,17 @@ def check_fermat(a, b, c, n):
         print('不，那样不行')
 
 
-check_fermat(1, 1, 2, 3)
+# check_fermat(1, 1, 2, 3)
+
+
+def tips():
+    print('a^n+b^n==c^n?')
+    a = int(input('a='))
+    b = int(input('b='))
+    c = int(input('c='))
+    n = int(input('n='))
+
+    check_fermat(a, b, c, n)
+
+
+tips()
